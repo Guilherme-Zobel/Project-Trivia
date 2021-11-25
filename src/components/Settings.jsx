0c3048380b826420f62fb805941164f4a764ed57
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-11-25 20:12:25 -0300

    3-requisito

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -4,7 +4,7 @@ class Settings extends React.Component {
   render() {
     return (
       <div data-testid="settings-title">
-        <h1>Configurações</h1>
+        <h2>Configurações</h2>
       </div>
     );
   }
