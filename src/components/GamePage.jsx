e38e05d15f4ad4e2e76ee4c2541a5347202f610f
Matheus Burthon <matatiaspatrizio@hotmail.com>
2021-11-26 22:41:47 -0300

    feito os requisitos 5 e 6.

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import TriviaGame from './TriviaGame';
 // import Settings from './Settings';
 
 class GamePage extends React.Component {
@@ -7,6 +8,7 @@ class GamePage extends React.Component {
     return (
       <div>
         <Header />
+        <TriviaGame />
       </div>
     );
   }
