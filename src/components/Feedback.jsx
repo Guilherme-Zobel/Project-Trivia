2305af5d9d7db41e7d8119b7d3af9e557d9f274e
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-12-03 20:34:08 -0300

    12-requeriment

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 export default class Feedback extends Component {
   render() {
+    const image = JSON.parse(localStorage.getItem('token'));
+    const state = JSON.parse(localStorage.getItem('state'));
     return (
       <div data-testid="feedback-text">
-        Feedback
+        <img data-testid="header-profile-picture" src={ `https://www.gravatar.com/avatar/${image}` } alt="imagem do usuário" />
+        <h2 data-testid="header-player-name">{ state.player.name }</h2>
+        <h3 data-testid="header-score">{ state.player.score }</h3>
       </div>
     );
   }
